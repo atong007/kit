899587db8553316d2da65cc1f55e32ce41a0139a
charlie <atong@88.com>
2022-09-07 14:49:24 +0800

    feat: log with zip

---
+++ log/log.go
@@ -0,0 +1,10 @@
+package log
+
+type Logger interface {
+	Debug(args ...interface{})
+	Info(args ...interface{})
+	Error(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+}
---
+++ log/std_log.go
@@ -0,0 +1,40 @@
+package log
+
+import (
+	"go.uber.org/zap"
+)
+
+type StdLog struct {
+	log *zap.SugaredLogger
+}
+
+func New() *StdLog {
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync()
+	sugar := logger.Sugar()
+	return &StdLog{log: sugar}
+}
+
+func (l *StdLog) Debug(args ...interface{}) {
+	l.log.Debug(args...)
+}
+
+func (l *StdLog) Info(args ...interface{}) {
+	l.log.Info(args...)
+}
+
+func (l *StdLog) Error(args ...interface{}) {
+	l.log.Error(args...)
+}
+
+func (l *StdLog) Debugf(format string, args ...interface{}) {
+	l.log.Debugf(format, args...)
+}
+
+func (l *StdLog) Infof(format string, args ...interface{}) {
+	l.log.Infof(format, args...)
+}
+
+func (l *StdLog) Errorf(format string, args ...interface{}) {
+	l.log.Errorf(format, args...)
+}
