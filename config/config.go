e50f86646bc873a5303241c381bf12071edf1857
charlie <atong@88.com>
2022-09-07 14:49:02 +0800

    feat: config loading

---
+++ config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+}
+
+func LoadAndRead[T json.Unmarshaler](file string) (*T, error) {
+	viper.SetConfigFile(file)
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("error loading config file: %w", err)
+	}
+	var conf T
+	if err := viper.Unmarshal(&conf); err != nil {
+		return nil, fmt.Errorf("error unmarshal config file: %w", err)
+	}
+	return &conf, nil
+}
